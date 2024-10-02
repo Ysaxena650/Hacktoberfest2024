205fc84efbcd9db724eb1c3b50015bffbecfa599
utkarsh tiwari <158811469+cosmickdd@users.noreply.github.com>
2024-10-03 03:00:29 +0530

    added my name to contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 427,
+    fullname: "Utkarsh Tiwari",
+    username: "https://github.com/cosmickdd",
+  },
   
 ];
